ba5afa6dc951e3ffebe02e0da4c24f99d60a7afe
lmmolinabe92 <lmmolinabe92@gmail.com>
2023-02-22 18:14:27 +0100

    Agregar archivos de proyecto.

---
+++ Cuenta.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio5
+{
+    public class Cuenta
+    {
+        protected double saldo;
+        protected int ni, nr;
+        protected double tasaAnual;
+        protected double comisionMensual;
+        public bool correcto = true;
+
+        public double Saldo
+        {
+            get => saldo;
+            protected set
+            {
+                if (value >= 0)
+                    saldo = value;
+                else
+                {
+                    Console.WriteLine("Error saldo negativo");
+                    correcto = false;
+                }
+            }
+        }
+
+        protected double TasaAnual
+        {
+            get => tasaAnual; set
+            {
+                if (value >= 0 && value < 15)
+                    tasaAnual = value;
+                else
+                {
+                    correcto = false;
+                    Console.WriteLine("Error Tasa no valida");
+                }
+            }
+        }
+
+        public Cuenta(double saldo, double tasaAnual)
+        {
+            Saldo = saldo;
+            TasaAnual = tasaAnual;
+            ni = 0;
+            nr = 0;
+            comisionMensual = 0;
+        }
+
+        public void Ingresar(double cant)
+        {
+            saldo += cant;
+            ni++;
+        }
+        public void Retirar(double cant)
+        {
+            Saldo -= cant;
+            nr++;
+        }
+
+        protected void InteresMensual()
+        {
+            comisionMensual = saldo * (tasaAnual / 12 / 100);
+            comisionMensual = Math.Round(comisionMensual, 2);
+            saldo -= comisionMensual;
+        }
+
+        public void ExtractoMensual()
+        {
+            Console.Write("Saldo antes de comision " + saldo);
+            InteresMensual();
+            Console.WriteLine(" se le ha cobrado " + comisionMensual + ", saldo actual " + saldo);
+        }
+
+        public override string ToString()
+        {
+            return "Su saldo es " + saldo + ". Ingresos: " + ni + " Retiros: " + nr;
+        }
+    }
+
+    public class Ahorro : Cuenta
+    {
+        private bool activo;
+        public Ahorro(double saldo, double tasaAnual) : base(saldo, tasaAnual)
+        {
+            activo = saldo >= 100;
+        }
+        public new void Retirar(double cant)
+        {
+            if (activo)
+            {
+                base.Retirar(cant);
+                activo = saldo >= 100;
+            }
+        }
+        private new void InteresMensual()
+        {
+            comisionMensual = saldo * (tasaAnual / 12 / 100);
+            comisionMensual = Math.Round(comisionMensual, 2);
+            saldo -= comisionMensual;
+            if (nr > 4)
+            {
+                comisionMensual += (nr - 4) * 1.5;
+            }
+        }
+        public new void ExtractoMensual()
+        {
+            Console.Write("Saldo antes de comision " + saldo);
+            InteresMensual();
+            Console.WriteLine(" se le ha cobrado " + comisionMensual + ", saldo actual " + saldo);
+            Console.WriteLine("Su cuenta " + (activo ? "" : "no ") + "esta activa.");
+        }
+
+        public override string ToString()
+        {
+            return "Su saldo es " + saldo + ", comision de " + comisionMensual + ", numero de transacciones " + (ni + nr);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Desarrollar un programa que modele una cuenta bancaria que tiene los siguientes atributos, que deben ser de acceso protegido:
+• Saldo, de tipo float.
+• Número de ingresos con valor inicial cero, de tipo int.
+• Número de retiros con valor inicial cero, de tipo int.
+• Tasa anual (porcentaje), de tipo float.
+• Comisión mensual con valor inicial cero, de tipo float.
+La clase Cuenta tiene un constructor que inicializa los atributos saldo y tasa anual con valores pasados como parámetros. La clase
+Cuenta tiene los siguientes métodos:
+• Ingresar una cantidad de dinero en la cuenta actualizando su saldo.
+• Retirar una cantidad de dinero en la cuenta actualizando su saldo. El valor a retirar no debe superar el saldo.
+• Calcular el interés mensual de la cuenta y actualiza el saldo correspondiente.
+• Extracto mensual: actualiza el saldo restándole la comisión mensual y calculando el interés mensual correspondiente (invoca el
+método anterior).
+• Imprimir: muestra en pantalla los valores de los atributos.
+
+La clase Cuenta tiene dos clases hijas:
+• Cuenta de ahorros: posee un atributo para determinar si la cuenta de ahorros está activa (tipo boolean). Si el saldo es menor a
+100€, la cuenta está inactiva, en caso contrario se considera activa. Los siguientes métodos se redefinen:
+1. Ingresar: se puede ingresar dinero si la cuenta está activa. Debe invocar al método heredado.
+2. Retirar: es posible retirar dinero si la cuenta está activa. Debe invocar al método heredado.
+3. Extracto mensual: si el número de retiros es mayor que 4, por cada retiro adicional, se cobra 1.5€ como comisión
+mensual. Al generar el extracto, se determina si la cuenta está activa o no con el saldo.
+4. Un nuevo método imprimir que muestra en pantalla el saldo de la cuenta, la comisión mensual y el número de
+transacciones realizadas (suma de cantidad de consignaciones y retiros).
+
+• Cuenta corriente: posee un atributo de sobregiro, el cual se inicializa en cero. Se redefinen los siguientes métodos:
+• Retirar: se retira dinero de la cuenta actualizando su saldo. Se puede retirar dinero superior al saldo. El dinero que se debe
+queda como sobregiro.
+• Ingresar: invoca al método heredado. Si hay sobregiro, la cantidad consignada reduce el sobregiro.
+• Extracto mensual: invoca al método heredado.
+• Un nuevo método imprimir que muestra en pantalla el saldo de la cuenta, la comisión mensual, el número de
+transacciones realizadas (suma de cantidad de consignaciones y retiros) y el valor de sobregiro.
+Realizar un método main que implemente un objeto Cuenta de ahorros y llame a los métodos correspondientes
+             */
+            Cuenta c;
+            do
+            {
+                Console.WriteLine("Introduzca el saldo de su nueva cuenta:");
+                double saldo = double.Parse(Console.ReadLine());
+                Console.WriteLine("Introduzca el interes anual:");
+                double interes = double.Parse(Console.ReadLine());
+                c = new Cuenta(saldo, interes);
+            } while (!c.correcto);
+            string r;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Opciones del programa:\n" +
+                    "1- Hacer Ingreso\n" +
+                    "2- Hacer Retiro\n" +
+                    "3- Extracto Meensual\n" +
+                    "4- Salir");
+                r = Console.ReadLine();
+                switch (r)
+                {
+                    case "1":
+                        Console.WriteLine("Introduzca una cantidad:");
+                        c.Ingresar(double.Parse(Console.ReadLine()));
+                        Console.WriteLine(c);
+                        Console.ReadLine();
+                        break;
+                    case "2":
+                        Console.WriteLine("Introduzca una cantidad:");
+                        c.Retirar(double.Parse(Console.ReadLine()));
+                        Console.WriteLine(c);
+                        Console.ReadLine();
+                        break;
+                    case "3":
+                        c.ExtractoMensual();
+                        Console.ReadLine();
+                        break;
+                    case "4":
+                        Console.WriteLine("Saliendo de la aplicacion");
+                        break;
+                    default:
+                        Console.WriteLine("Seleccion incorrecta");
+                        break;
+                }
+
+            } while (r != "4");
+
+
+            Cuenta a = new Ahorro(500, 12);
+            a.ExtractoMensual();
+
+
+
+            Console.ReadLine();
+        }
+        
+    }
+}
